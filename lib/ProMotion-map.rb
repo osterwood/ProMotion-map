43f43f8f000488a4534be9df6d9554a2c75f1096
Chris Osterwood <osterwood@gmail.com>
2016-02-18 13:54:05 -0500

    Update ProMotion-map.rb

--- lib/ProMotion-map.rb
+++ lib/ProMotion-map.rb
@@ -3,11 +3,19 @@ unless defined?(Motion::Project::Config)
   raise "ProMotion-map must be required within a RubyMotion project."
 end
 
+require 'motion-require'
+
+lib_dir_path = File.dirname(File.expand_path(__FILE__))
+
+files = []
+files << File.join(lib_dir_path, "ProMotion/map/map_screen_annotation.rb")
+files << File.join(lib_dir_path, "ProMotion/map/map_screen_module.rb")
+files << File.join(lib_dir_path, "ProMotion/map/map_screen.rb")
+
+Motion::Require.all(files)
+
 Motion::Project::App.setup do |app|
-  lib_dir_path = File.dirname(File.expand_path(__FILE__))
-  app.files << File.join(lib_dir_path, "ProMotion/map/map_screen_annotation.rb")
-  app.files << File.join(lib_dir_path, "ProMotion/map/map_screen_module.rb")
-  app.files << File.join(lib_dir_path, "ProMotion/map/map_screen.rb")
+  app.detect_dependencies = false
 
   app.frameworks += %w(CoreLocation MapKit)
 end
